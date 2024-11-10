2b14b795ff1fc362b10ea139d917a64d536a8239
Kopchan <83940856+Kopchan@users.noreply.github.com>
2024-11-11 03:26:27 +0700

    Фикс валидации альбома

--- api/app/Http/Requests/Api/Album/AlbumUpdateRequest.php
+++ api/app/Http/Requests/Api/Album/AlbumUpdateRequest.php
@@ -3,15 +3,33 @@
 namespace App\Http\Requests\Api\Album;
 
 use App\Http\Requests\Api\ApiRequest;
+use App\Models\Album;
+use GuzzleHttp\Psr7\Request;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class AlbumUpdateRequest extends ApiRequest
 {
     public function rules(): array
     {
+        $album = $this->route('album');
+        if ($album instanceof Album) $album = $album->id;
         return [
-            'name' => 'string|max:255',
-            'path' => 'nullable|string|max:255',
+            'name' => [
+                'string',
+                'max:255',
+                Rule::unique(Album::class, 'name')
+                    ->where('user_id', auth()->id())
+                    ->ignore($album),
+            ],
+            'path' => [
+                'nullable',
+                'string',
+                'max:255',
+                Rule::unique(Album::class, 'path')
+                    ->where('user_id', auth()->id())
+                    ->ignore($album),
+            ],
         ];
     }
 }
