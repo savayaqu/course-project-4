ced4ba7427f6b24185f5594af003c0e773241abf
savayaqu <zcomn@mail.ru>
2024-11-24 21:48:55 +0700

    Айоу

---
+++ PicsyncAdmin/PicsyncAdmin/Views/Home.xaml.cs
@@ -0,0 +1,19 @@
+using PicsyncAdmin.Models;
+
+namespace PicsyncAdmin.Views;
+
+public partial class Home : ContentPage
+{
+	private User _user;
+	private string _token;
+	public Home(User user, string token)
+	{
+		InitializeComponent();
+		_user = user;
+		_token = token;
+
+		//Устанавливаем аватар и никнейм
+		LoginHome.Text = _user.Login;
+
+    }
+}
